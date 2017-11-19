529ed34d6887cd89bef25370bb78d151fde0714d
Generian <sebastian.guenther@ph.tum.de>
2017-11-19 18:22:28 +0100

    Add mobile config v2.

--- js/gun.js
+++ js/gun.js
@@ -11,8 +11,8 @@ function Gun() {
 
   this.update = function() {
     //var pos = mouseX;
-    console.log(touches[0]);
-    var pos = mouseX;
+    //console.log(touches[0]);
+    var pos = touches[0];
     pos = pos - (this.length / 2.0);
     this.x = constrain(pos, 0 , width - this.length);
   }
